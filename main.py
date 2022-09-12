531495c7a8b4044075191ba77a07d679793ab0b7
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-09-12 14:01:04 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+allAccounts = ["1212", "1215", "99123"]
+while True:
+    print("1. Lista alla konton")
+    print("2. Skriv ut hur många konton")
+    print("3. Lägg till konto")
+    print("4. Ta bort konto")
+    print("5. Söka")
+    action = input("Ange val:")
+    if action == "2":
+        print(f"Antal konton:{len(allAccounts)}")
+    elif action == "1":
+        for namn in allAccounts:
+            print(namn)
+    elif action == "3":
+        namn = input("Ange namn på nya kontot")
+        allAccounts.append(namn)
+    elif action == "4":
+        for index in range(0, len(allAccounts)):
+            print(f"{index+1} {allAccounts[index]}")
+
+        index = int(input("Ange nummer på kontot som ska tas bort:"))
+        index = index -1
+        del allAccounts[index]
+
+    elif action == "5":
+        searchQuery = input("Sök efter:").lower()
+        for namn in allAccounts:
+            lowerCaseNamn = namn.lower()
+            index = lowerCaseNamn.find(searchQuery)
+            if index != -1:
+                print(namn)
+
